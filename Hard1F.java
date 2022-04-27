d00637088bb3b9fa99806a3af50a5555d5c63e18
Kyae <50015990+JohndKim@users.noreply.github.com>
2022-04-27 15:57:22 +0800

    Add files via upload

---
+++ Hard1F.java
@@ -0,0 +1,12 @@
+public class Hard1F {
+    public static void main(String[] args){
+        int a = 6;
+        int b = 3;
+
+        if (b == 0){
+            System.out.println("NaN");
+        } else {
+            System.out.println(a/b);
+        }
+    }
+}
